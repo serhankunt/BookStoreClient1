7c1c38fd43dc479187c58f8e18f1b215a1ea9715
serhankunt <hserhan006@gmail.com>
2023-10-19 13:26:54 +0300

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  books:number[] = [1,2,3,4,5,6];
+  response : any ;
+  pageNumbers : number[] = [];
+  
+  //https://localhost:7117/1/10
+
+  constructor(private http:HttpClient){ 
+    this.getAll();
+  }
+
+  getAll(pageNumber=1){
+    this.http.get(`https://localhost:7117/${pageNumber}/10`)
+    .subscribe(res=>{
+      this.response = res;
+      this.setPageNumber();
+    })
+  }
+
+  setPageNumber(){
+    this.pageNumbers = [];
+    for(let i = 0; i <this.response.totalPageCount; i++){
+      this.pageNumbers.push(i+1);
+    }
+    console.log(this.pageNumbers);
+  }
 }
